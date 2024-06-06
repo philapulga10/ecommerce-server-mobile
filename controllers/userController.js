7ce8e191a95b6830a24ac0373a609f113fa481e9
philapulga10 <phinguyenuit10@gmail.com>
2024-06-06 18:20:05 +0700

    handle login

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,3 +52,47 @@ export const registerController = async (req, res) => {
     });
   }
 };
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: "Please add email or password",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(500).send({
+        success: false,
+        message: "invalid credentials",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      success: false,
+      message: "Error in login API",
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 
-import { registerController } from "../controllers/userController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/userController.js";
 
 const router = express.Router();
+
+router.post("/login", loginController);
 
 router.post("/register", registerController);
 
